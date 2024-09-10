58b2b580e980b67d6bc5fb942f1d5767e8530751
Maruiti <maruiti1@gmail.com>
2024-09-10 18:57:49 -0300

    configuração da tela de recuperação de senha

--- lib/esqueceu_senha.dart
+++ lib/esqueceu_senha.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
-
-class SegundaTela extends StatelessWidget {
+//definição de nome de tela para chamada
+class SegundaTela extends StatefulWidget {
   const SegundaTela({super.key});
+
+  @override
+  _SegundaTelaState createState() => _SegundaTelaState();
+}
+
+class _SegundaTelaState extends State<SegundaTela> {
+  final _emailController = TextEditingController();
+
+  void _showRecoveryDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confira sua caixa de entrada'),
+          content: const Text('Instruções de recuperação de senha foram enviadas para o seu email.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Fecha o diálogo
+                Navigator.pop(context); // Volta para a tela de login
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +38,48 @@ class SegundaTela extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Esqueceu sua senha?'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context); // Voltar para a tela de login
-          },
-          child: const Text('Voltar para login'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Digite seu email para recuperação de senha',
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: 'Digite seu Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor, insira seu email';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            MaterialButton(
+              minWidth: double.infinity,
+              color: const Color.fromARGB(255, 81, 177, 84),
+              textColor: Colors.white,
+              child: const Text('Confirmar'),
+              onPressed: () {
+                if (_emailController.text.isNotEmpty) {
+                  _showRecoveryDialog();
+                } else {
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
