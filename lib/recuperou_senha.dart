1f3346e2342cb7612913b11a4d231d11a4d91393
Maruiti <maruiti1@gmail.com>
2024-09-10 11:12:05 -0300

    Trabalhando nas senhas

--- lib/recuperou_senha.dart
+++ lib/recuperou_senha.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class RecuperaSenha extends StatelessWidget {
+  const RecuperaSenha({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
