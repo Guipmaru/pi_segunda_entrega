42b92b99da939a816ca8f04462837fdaa85bdea8
Maruiti <maruiti1@gmail.com>
2024-09-11 17:03:31 -0300

    Correção da navegação da homepage

--- lib/iniciando_a_homepage.dart
+++ lib/iniciando_a_homepage.dart
@@ -1,115 +1,134 @@
 import 'package:flutter/material.dart';
-import 'package:pi_segunda_entrega/data/database_helper.dart';
+import 'package:pi_segunda_entrega/confere_agendamento.dart';
+import 'package:pi_segunda_entrega/data/database_helper.dart'; 
+import 'formulario.dart';
+import 'perfil.dart';
+import 'confere_agendamento.dart';
+import 'buscar_ponto.dart';
 
-class Homepage extends StatefulWidget{
+
+
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
   @override
   HomepageState createState() => HomepageState();
 }
-  
+
+class HomepageState extends State<Homepage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 208, 241, 209),
-        body: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: const Color.fromARGB(255, 208, 241, 209),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-        )
-        title: const Text(
-          'Como é bom te ver aqui,'
-          style: TextStyle(
-            fontSize: 35,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+          const Text(
+            'Como é bom te ver aqui,',
+            style: TextStyle(
+              fontSize: 35,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-          ), // título da homepage
-        ),
+          const SizedBox(height: 20), // Espaçamento entre o título e o texto
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 100),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Registro da sua última doação: 15/12/2023'),
+                SizedBox(height: 10),
+                Text('Próxima doação prevista: [data da próxima doação]'),
+                SizedBox(height: 10),
+                Text('O que deseja fazer hoje?'),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20), // Espaçamento entre o texto e os botões
 
-         Padding(
-          padding: const EdgeInsets.symetric(horizontal: 100),
-          child: Text(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text('Registro da sua última doação: 15/12/2023'),
-            SizedBox(height: 10),
-            Text('Próxima doação prevista: [data da próxima doação'),
-            SizedBox(height: 10),
-            Text('O que deseja fazer hoje?'),
-          ], //informações do paciente e texto para o menu de botões
-        ),
-      ),
 
-           Padding(
-            padding: const EdgeInsets.symetric(horizontal: 150),
+          //construção do navegador push para a tela de perfil para atualização de dados  
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 150),
             child: MaterialButton(
-            mainAnxisAlignment: MainAxisAlignment.center,
-            color: const Color.fromARGB(255, 81, 177, 84),
-            textColor: Colors.white,
-            child: const Text('Atualizar meus Dados para doação de sangue'),
+              color: const Color.fromARGB(255, 81, 177, 84),
+              textColor: Colors.white,
+              child: const Text('Atualizar meus Dados para doação de sangue'),
               onPressed: () {
-               Navigator.push(
-                context,
-               MaterialPageRoute(
-                builder: (context) => TelaFormulario(), // inserir a tela correta
-              ), // botão de atualizar os dados para a doação de sangue
-            );
-          },
-        ),
-      ),
-            Padding(
-             padding: const EdgeInsets.symetric(horizontal: 150),
-             child: MaterialButton(
-             mainAnxisAlignment: MainAxisAlignment.center,
-             color: const Color.fromARGB(255, 81, 177, 84),
-             textColor: Colors.white,
-             child: const Text('Agendar minha doação de sangue'),
-               onPressed: () {
                 Navigator.push(
-                 context,
-                MaterialPageRoute(
-                 builder: (context) => TelaAgendamento(), // inserir a tela correta
-              ), // botão para agendamento da doação
-            );
-          },
-        ),
-      ),
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TelaPerfil(), 
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 10), // Espaçamento entre os botões
 
-            Padding(
-             padding: const EdgeInsets.symetric(horizontal: 150),
-             child: MaterialButton(
-             mainAnxisAlignment: MainAxisAlignment.center,
-             color: const Color.fromARGB(255, 81, 177, 84),
-             textColor: Colors.white,
-             child: const Text('Trocar o agendamento da doação de sangue'),
+          //navegador push para a tela de formulario de agendamento de doação
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 150),
+            child: MaterialButton(
+              color: const Color.fromARGB(255, 81, 177, 84),
+              textColor: Colors.white,
+              child: const Text('Agendar minha doação de sangue'),
               onPressed: () {
-               Navigator.push(
-                context,
-               MaterialPageRoute(
-                builder: (context) => TelaTrocadeAgendamento(), // inserir a tela correta
-              ), // botão de trocar agendamento da doação de sangue
-            );
-          },
-        ),
-      ),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TelaFormulario(),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 10), // Espaçamento entre os botões
 
-            Padding(
-             padding: const EdgeInsets.symetric(horizontal: 150),
-             child: MaterialButton(
-             mainAnxisAlignment: MainAxisAlignment.center,
-             color: const Color.fromARGB(255, 81, 177, 84),
-             textColor: Colors.white,
-             child: const Text('Buscar pontos para doação de sangue'),
+          //Navegação para tela de conferência ou  troca de agendamento
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 150),
+            child: MaterialButton(
+              color: const Color.fromARGB(255, 81, 177, 84),
+              textColor: Colors.white,
+              child: const Text('Trocar o agendamento da doação de sangue'),
               onPressed: () {
-               Navigator.push(
-                context,
-               MaterialPageRoute(
-                builder: (context) => TelaFormulario(), // inserir a tela correta
-              ), // botão para mostrar os pontos de doações
-            );
-          },
-        ),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ConfereAgendamento(), 
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 10), // Espaçamento entre os botões
+
+
+          //Tela para buscar pontos de agendamento
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 150),
+            child: MaterialButton(
+              color: const Color.fromARGB(255, 81, 177, 84),
+              textColor: Colors.white,
+              child: const Text('Buscar pontos para doação de sangue'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TelaFormulario(), 
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
 
 void main() {
   runApp(const MaterialApp(
--- lib/perfil.dart
+++ lib/perfil.dart
@@ -1,1 +1,35 @@
 import 'package:flutter/material.dart';
+import 'iniciando_a_homepage.dart'; 
+import 'package:pi_segunda_entrega/data/database_helper.dart';
+
+class TelaPerfil extends StatelessWidget {
+  const TelaPerfil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Perfil'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Dados pessoais',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // salvar os dados do formulário (falta implementar)
+                Navigator.pop(context); // Volta para a tela anterior (Homepage)
+              },
+              child: const Text('voltar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
