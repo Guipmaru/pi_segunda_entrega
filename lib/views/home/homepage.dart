b9b733ed3c0af481477895fbb7c7759fbb2e7b93
Paola Akemi <akemi-silva@hotmail.com>
2024-09-14 19:57:07 -0300

    homepage.dart

--- lib/views/home/homepage.dart
+++ lib/views/home/homepage.dart
@@ -1,69 +1,170 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; 
-import 'package:pi_segunda_entrega/models/user_model.dart';
 import 'package:pi_segunda_entrega/controllers/user_controller.dart';
-import 'package:pi_segunda_entrega/views/profile/perfil.dart'; 
-import 'package:pi_segunda_entrega/views/date/confere_agendamento.dart'; 
-import 'package:pi_segunda_entrega/views/date/agendamento_doacao.dart'; 
-import 'package:pi_segunda_entrega/views/local/local_doacao.dart'; 
-import 'package:pi_segunda_entrega/data/database_helper.dart'; 
+import 'package:pi_segunda_entrega/data/database_helper.dart';
+import 'package:pi_segunda_entrega/views/home/homepage.dart';
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+class ConfereAgendamento extends StatefulWidget {
+  const ConfereAgendamento({super.key});
 
   @override
-  HomepageState createState() => HomepageState();
+  ConfereAgendamentoState createState() => ConfereAgendamentoState();
 }
 
-class HomepageState extends State<Homepage> {
-  String firstName = '';
-  String lastName = '';
-  String nextDonationDate = 'Você não possui doação agendada';
+class ConfereAgendamentoState extends State<ConfereAgendamento> {
+  DateTime? selectedDate;
+  TimeOfDay? selectedTime;
+  String appointmentLocation = '';
   final UserController _userController = UserController();
   final DatabaseHelper _databaseHelper = DatabaseHelper();
 
   @override
   void initState() {
     super.initState();
-    _loadUserData();
+    _loadAppointmentData();
   }
 
-  Future<void> _loadUserData() async {
-    User? user = await _userController.getUsuarioLogado();
+  Future<void> _loadAppointmentData() async {
+    var user = await _userController.getUsuarioLogado();
     if (user != null) {
-      setState(() {
-        firstName = user.firstName;
-        lastName = user.lastName;
-      });
-
       var nextAppointment = await _databaseHelper.getAgendamento(user.id);
-      
       if (nextAppointment != null) {
         setState(() {
-          try {
-            // formato da data dia/mês/ano
-            DateFormat inputFormat = DateFormat('dd-MM-yyyy');
-            DateTime dateTime = inputFormat.parse(nextAppointment['data']);
-            
-            DateFormat outputFormat = DateFormat('dd-MM-yyyy');
-            String formattedDate = outputFormat.format(dateTime);
-            
-            // Incluindo o local no texto
-            nextDonationDate = 'Próxima doação prevista: $formattedDate às ${nextAppointment['hora']} no local: ${nextAppointment['local']}';
-          } catch (e) {
-            print('Error parsing date: $e');
-            nextDonationDate = 'Erro ao formatar a data do agendamento';
-          }
+          selectedDate = DateTime.parse(nextAppointment['data']);
+          selectedTime = TimeOfDay.fromDateTime(DateTime.parse(nextAppointment['hora']));
+          appointmentLocation = nextAppointment['local'];
         });
       } else {
-        setState(() {
-          nextDonationDate = 'Você não possui doação agendada';
-        });
+        _showNoAppointmentMessage();
       }
     }
   }
-  void _logout() {
-    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+
+  Future<void> _updateAppointment(DateTime newDate, TimeOfDay newTime, String newLocation) async {
+    if (newLocation.isEmpty) {
+      _showMessage('Por favor, informe o novo local.', false);
+      return;
+    }
+
+    var user = await _userController.getUsuarioLogado();
+    if (user != null) {
+      await _databaseHelper.updateAgendamento(
+        user.id,
+        newDate.toIso8601String().split('T')[0], // Formata a data no formato YYYY-MM-DD
+        newTime.format(context), // Formata a hora no formato HH:mm
+        newLocation,
+      );
+      _showMessage('Seu agendamento foi alterado com sucesso!', true);
+    }
+  }
+
+  Future<void> _cancelAppointment() async {
+    var user = await _userController.getUsuarioLogado();
+    if (user != null) {
+      await _databaseHelper.deleteAgendamento(user.id);
+      _showMessage('Seu agendamento foi cancelado!', true);
+    }
+  }
+
+  void _showMessage(String message, bool redirectToHome) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                if (redirectToHome) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Homepage()),
+                  );
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showNoAppointmentMessage() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: const Text('Nenhum agendamento encontrado.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Homepage()),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _confirmCancel() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: const Text('Tem certeza que deseja cancelar seu agendamento?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Não'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Sim'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _cancelAppointment();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _selectDate() async {
+    DateTime initialDate = selectedDate ?? DateTime.now();
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: initialDate,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2100),
+    );
+    if (pickedDate != null && pickedDate != initialDate) {
+      setState(() {
+        selectedDate = pickedDate;
+      });
+    }
+  }
+
+  Future<void> _selectTime() async {
+    TimeOfDay initialTime = selectedTime ?? TimeOfDay.now();
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: initialTime,
+    );
+    if (pickedTime != null && pickedTime != initialTime) {
+      setState(() {
+        selectedTime = pickedTime;
+      });
+    }
   }
 
   @override
@@ -71,69 +172,15 @@ class HomepageState extends State<Homepage> {
     double screenWidth = MediaQuery.of(context).size.width * 0.8;
 
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 190, 228, 191),
+      backgroundColor: const Color.fromARGB(255, 238, 159, 155),
       appBar: AppBar(
-        title: const Text('Bem-vindo'),
-        backgroundColor: const Color.fromARGB(255, 81, 177, 84),
-        
+        title: const Text('Trocar ou Cancelar Agendamento'),
+        backgroundColor: const Color.fromARGB(255, 212, 14, 14),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(height: 10),
-
-            // logo do projeto
-            Image.asset(
-              'lib/assets/images/logotipo-semfundo.png', // Caminho da imagem
-              width: 200, // Largura da imagem
-              height: 200, // Altura da imagem
-            ),
-            const SizedBox(height: 0),
-
-            // Texto de boas-vindas
-            Text.rich(
-              TextSpan(
-                text: 'Como é bom te ver aqui,',
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                children: <TextSpan>[
-                  const TextSpan(text: '\n'),
-                  TextSpan(
-                    text: '$firstName $lastName',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: screenWidth,
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 217, 235, 206),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Registro da sua última doação:',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  const SizedBox(height: 10),
-                  Text(nextDonationDate,
-                      style: const TextStyle(fontWeight: FontWeight.bold)),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
             Container(
               width: screenWidth,
               padding: const EdgeInsets.all(10),
@@ -142,11 +189,68 @@ class HomepageState extends State<Homepage> {
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  _buildButton(context, 'Atualizar meus Dados para doação de sangue', const TelaPerfil(), screenWidth),
-                  _buildButton(context, 'Agendar minha doação de sangue', AgendamentoScreen(), screenWidth),
-                  _buildButton(context, 'Trocar o agendamento da doação de sangue', const ConfereAgendamento(), screenWidth),
-                  _buildButton(context, 'Buscar pontos para doação de sangue', LocalDoacaoPage(), screenWidth),
+                  Text(
+                    'Sua doação será marcada para: ${selectedDate?.toLocal().toString().split(' ')[0] ?? ''} ${selectedTime?.format(context) ?? ''}\nLocal: $appointmentLocation',
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _selectDate,
+                    child: const Text(
+                      'Selecionar Nova Data',
+                      style: TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromARGB(255, 16, 27, 133),
+                                  fontWeight: FontWeight.bold
+                                ),
+                                ),
+                  ),
+                  ElevatedButton(
+                    onPressed: _selectTime,
+                    child: const Text(
+                      'Selecionar Novo Horário',
+                      style: TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromARGB(255, 16, 27, 133),
+                                  fontWeight: FontWeight.bold
+                                ),
+                                ),
+                  ),
+                  _buildTextField('Novo Local'),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          if (selectedDate != null && selectedTime != null && appointmentLocation.isNotEmpty) {
+                            _updateAppointment(
+                              selectedDate!,
+                              selectedTime!,
+                              appointmentLocation,
+                            );
+                          } else {
+                            _showMessage('Por favor, selecione uma data, hora e local.', false);
+                          }
+                        },
+                        child: const Text('Trocar'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 81, 177, 84),
+                          foregroundColor: Colors.white,
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: _confirmCancel,
+                        child: const Text('Desmarcar'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -156,23 +260,18 @@ class HomepageState extends State<Homepage> {
     );
   }
 
-  Widget _buildButton(BuildContext context, String label, Widget page, double width) {
+  Widget _buildTextField(String label) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5),
-      child: SizedBox(
-        width: width,
-        child: MaterialButton(
-          color: const Color.fromARGB(255, 81, 177, 84),
-          textColor: Colors.white,
-          child: Text(label),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => page,
-              ),
-            );
-          },
+      child: TextField(
+        onChanged: (value) {
+          setState(() {
+            appointmentLocation = value;
+          });
+        },
+        decoration: InputDecoration(
+          labelText: label,
+          border: const OutlineInputBorder(),
         ),
       ),
     );
