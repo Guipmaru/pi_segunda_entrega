c342031593018b937322799c31b7ab48bdfebbe4
Paola Akemi <akemi-silva@hotmail.com>
2024-09-14 19:53:58 -0300

    login_screen.dart

--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -48,137 +48,152 @@ class _LoginScreenState extends State<LoginScreen> {
       _passwordController.clear();
     });
   }
-
-  @override
+  
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 190, 228, 191),
-      resizeToAvoidBottomInset: true, 
-      body: SingleChildScrollView( 
+      appBar: AppBar(
+        title: const Text ('Login'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 212, 14, 14),
+      ),
+      backgroundColor: const Color.fromARGB(255, 238, 159, 155),
+      body: Container(
+        child:SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 80), 
-            Image.asset(
-              'lib/assets/images/logotipo-semfundo.png',
-              width: 200,
-              height: 200,
-            ),
-            const SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: [
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Adicionando uma imagem
+          Image.asset('lib/assets/images/logotipo-semfundo.png',
+            width: 300 ,
+            height: 300,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Digite seu Email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira o email';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Senha',
+                        hintText: 'Digite sua senha',
+                        prefixIcon: Icon(Icons.password),
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira a senha';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  if (_errorMessage.isNotEmpty)
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 50),
-                      child: TextFormField(
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          hintText: 'Digite seu Email',
-                          prefixIcon: Icon(Icons.email),
-                          border: OutlineInputBorder(),
-                          filled: true,
-                          fillColor: Colors.white,
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Por favor, insira o email';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                    const SizedBox(height: 30),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 50),
-                      child: TextFormField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelText: 'Senha',
-                          hintText: 'Digite sua senha',
-                          prefixIcon: Icon(Icons.password),
-                          border: OutlineInputBorder(),
-                          filled: true,
-                          fillColor: Colors.white,
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Por favor, insira a senha';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                    const SizedBox(height: 30),
-                    if (_errorMessage.isNotEmpty)
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 50),
-                        child: Column(
-                          children: [
-                            Text(
-                              _errorMessage,
-                              style: const TextStyle(color: Colors.red, fontSize: 16),
-                            ),
-                            ElevatedButton(
-                              onPressed: _clearError,
-                              child: const Text('OK'),
-                            ),
-                          ],
-                        ),
-                      ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 100),
-                      child: MaterialButton(
-                        minWidth: double.infinity,
-                        onPressed: _login,
-                        color: const Color.fromARGB(255, 81, 177, 84),
-                        textColor: Colors.white,
-                        child: const Text('Entrar'),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const EsqueceuSenha(),
-                              ),
-                            );
-                          },
-                          child: const Text('Esqueceu sua senha?'),
-                        ),
-                        const SizedBox(width: 10),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const CadastroUser(),
-                              ),
-                            );
-                          },
-                          child: const Text(
-                            'Cadastre-se',
-                            style: TextStyle(color: Color.fromARGB(255, 81, 177, 84)),
+                      child: Column(
+                        children: [
+                          Text(
+                            _errorMessage,
+                            style: const TextStyle(color: Colors.black, fontSize: 16),
                           ),
-                        ),
-                      ],
+                          ElevatedButton(
+                            onPressed: _clearError,
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 100),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      onPressed: _login,
+                      color: const Color.fromARGB(255, 212, 14, 14),
+                      textColor: Colors.white,
+                      child: const Text('Entrar'),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const EsqueceuSenha(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Esqueceu sua senha?',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 16, 27, 133),
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                      ),
+                      const SizedBox(width: 10),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const CadastroUser(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Cadastre-se',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 16, 27, 133),
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+      ),
       ),
     );
   }
